c6a5593d0222b8344428130694b28edde07c7222
Iker Bascones <iker.bascones-legarda@michelin.com>
2023-09-22 10:37:09 +0200

    Fix accesories error

--- routes/accesorio.js
+++ routes/accesorio.js
@@ -5,7 +5,7 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
-const { getAccesorio, updateAccesorio, createAccesorio, deleteAccesorio, getAccesorioOne } = require('../controllers/Accesorio');
+const { getAccesorio, updateAccesorio, createAccesorio, deleteAccesorio, getAccesorioOne } = require('../controllers/accesorio');
 
 const { 
     validarJWT, 
